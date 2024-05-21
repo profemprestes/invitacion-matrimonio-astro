085ea983904085cad24a9e03189933949f733401
alexanderdev <bryanalexandermoscolbravo@gmail.com>
2024-05-20 22:39:14 -0500

    agregando tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
+import vercelServerless from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react(), tailwind()],
+  output: 'server',
+  adapter: vercelServerless()
 });
