1399b26eea9b4f2bc2e574c460b5ff8c032d6723
Profe Matias Prestes <profematiasprestes@gmail.com>
2025-04-15 22:13:20 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,12 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import netlify from '@astrojs/netlify';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: 'server',
   adapter: netlify({
     edgeMiddleware: true,
-    // Desactivar completamente las redirecciones
     functionPerRoute: false,
-    redirects: false
+    redirects: false // Ensure this setting is correct
   })
 });
