b6372076a121b5847d2f43eab6dc50a26f92a697
Profe Matias Prestes <profematiasprestes@gmail.com>
2025-04-18 05:50:57 -0300

    Create .eslintrc.cjs

---
+++ .eslintrc.cjs
@@ -0,0 +1,61 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2022: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:astro/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'react', 'jsx-a11y', 'prettier'],
+  rules: {
+    'prettier/prettier': 'warn',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-uses-react': 'off',
+    'jsx-a11y/click-events-have-key-events': 'warn',
+    'jsx-a11y/no-static-element-interactions': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  overrides: [
+    {
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
+      },
+      rules: {
+        // Reglas específicas para archivos Astro
+      },
+    },
+    {
+      files: ['*.tsx', '*.jsx'],
+      rules: {
+        // Reglas específicas para archivos React
+      },
+    },
+  ],
+  ignorePatterns: ['dist/**', 'node_modules/**', '.astro/**', '*.cjs'],
+};
